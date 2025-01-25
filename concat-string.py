8fa1e1bcadfd9c88f3a3830529ed22b92e329c63
Jhean Antunes Lopes <120582814+JheanAntunes@users.noreply.github.com>
2025-01-25 13:57:45 +0000

    feat: add script para concatenar duas entradas de texto

---
+++ concat-string.py
@@ -0,0 +1,4 @@
+primeraEntrada = input('Digite a primeira entrada: ')
+segundaEntrada = input('Digite a primeira segunda: ')
+concatEntradas = primeraEntrada + ' ' + segundaEntrada
+print(concatEntradas)
